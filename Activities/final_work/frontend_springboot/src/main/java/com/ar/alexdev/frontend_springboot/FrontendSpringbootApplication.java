9a1e603fb92dfe0c683e4ce6cf2e7770c72c6aee
alex <alexxxd23@gmail.com>
2023-11-25 21:03:59 -0300

    add new frontend app

--- Activities/final_work/frontend_springboot/src/main/java/com/ar/alexdev/frontend_springboot/FrontendSpringbootApplication.java
+++ Activities/final_work/frontend_springboot/src/main/java/com/ar/alexdev/frontend_springboot/FrontendSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.ar.alexdev.frontendspringboot;
+package com.ar.alexdev.frontend_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Activities/final_work/frontend_springboot/src/test/java/com/ar/alexdev/frontend_springboot/FrontendSpringbootApplicationTests.java
+++ Activities/final_work/frontend_springboot/src/test/java/com/ar/alexdev/frontend_springboot/FrontendSpringbootApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ar.alexdev.frontendspringboot;
+package com.ar.alexdev.frontend_springboot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
